8c429cef76f322143f49ced0ef1a68e540627b1c
Rosita <roseoctober@hotmail.co.uk>
2017-06-10 15:54:04 +0100

    cleaned up code. removed all references to paths. now using links. removed redraw function.

--- app.js
+++ app.js
@@ -5,15 +5,15 @@ var barPadding = 1;
 var defaultEdgeLength = 50;
 var edgeLength = defaultEdgeLength;
 var square = 50;
-var nodeLimit = Math.floor((w - 2 * square) / (square + edgeLength)) + 1; // use for insert check
+var nodeLimit = Math.floor((w - 2 * square) / (square + edgeLength)) + 1;
 var topY = h / 3;
 var bottomY = 2*h/3;
 var maxValue = 25;
 var elements1 = [ 10, 15 ];
 var elements2 = [ 5, 10, 16, 19, 11 ];
 var elements3 = [ 5, 2, 25, 10, 18 ];
-// var elements3 = [ 5, 10, 16, 19, 11, 15, 20, 17 ];
-var elements = elements2;
+var elements4 = [ 5, 10, 16, 19, 11, 15, 20, 17 ];
+var elements = elements1.slice();
 var nodes = [];
 var edges = [];
 var squares;
@@ -25,17 +25,6 @@ var svg = d3.select("body")
             .append("svg")
             .attr("width", w)
             .attr("height", h);
-
-svg.append('svg:defs').append('svg:marker')
-    .attr('id', 'end-arrow')
-    .attr('viewBox', '0 -5 10 10')
-    .attr('refX', 6)
-    .attr('markerWidth', 3)
-    .attr('markerHeight', 3)
-    .attr('orient', 'auto')
-  .append('svg:path')
-    .attr('d', 'M0,-5L10,0L0,5')
-    .attr('fill', '#000');
 
 function convertData() {
   nodes = [];
@@ -57,14 +46,24 @@ function convertData() {
       }
       edges.push(edge);
     }
-    console.log("converting data");
     index++;
   });
 }
 
 function restart() {
-  //svg.selectAll("*").remove();
   convertData();
+  svg.selectAll("*").remove();
+
+  svg.append('svg:defs').append('svg:marker')
+      .attr('id', 'end-arrow')
+      .attr('viewBox', '0 -5 10 10')
+      .attr('refX', 6)
+      .attr('markerWidth', 3)
+      .attr('markerHeight', 3)
+      .attr('orient', 'auto')
+    .append('svg:path')
+      .attr('d', 'M0,-5L10,0L0,5')
+      .attr('fill', '#000');
 
   svg.selectAll("rect")
     .data(nodes)
@@ -74,7 +73,7 @@ function restart() {
       return d.xPos;
     })
     .attr("y", function(d) {
-      return d.yPos; // topY
+      return d.yPos;
     })
     .attr("width", square)
     .attr("height", square)
@@ -93,39 +92,17 @@ function restart() {
       return d.xPos + square/2;
     })
     .attr("y", function(d) {
-      return d.yPos + square/ 2 + 7 ; // d.yPos = topY
+      return d.yPos + square/ 2 + 7 ;
     })
     .attr("font-family", "sans-serif")
     .attr("font-size", "20px")
     .attr("fill", "white")
     .attr("text-anchor", "middle");
 
-  console.log(edges);
-/*
-  svg.append('svg:g').selectAll('path')
-  // svg.selectAll('path')
-    .data(edges)
-    .enter()
-    .append("path")
-    .style("marker-start", "")
-    .style("marker-end", "url(#end-arrow)")
-    .style("stroke", "black")
-    .style("stroke-width", "5px")
-    .style("fill", "none")
-    .attr("d", function(d, i) {
-      console.log(i);
-      console.log(d);
-      var sourceX = d.source.xPos + square;
-      var sourceY = d.source.yPos + square / 2;
-      var targetX = d.target.xPos;
-      var targetY = d.target.yPos + square / 2;
-      return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
-    });*/
     svg.selectAll("line")
       .data(edges)
       .enter()
       .append("line")
-      // .style("marker-start", "")
       .style("marker-end", "url(#end-arrow)")
       .style("stroke", "black")
       .style("stroke-width", "5px")
@@ -149,7 +126,6 @@ function frame() {
 }
 
 function addNode() {
-  console.log("addNode clicked");
   if (elements.length >= nodeLimit) {
     console.log("max limit reached");
     // if (duplicate) "no duplicates allowed" ?
@@ -160,16 +136,12 @@ function addNode() {
     updateData();
     enterElem();
     updateVisuals();
-    // restart();
-    // console.log(edges);
-    // console.log(nodes);
   }
 }
 
 function updateData() {
   squares = svg.selectAll("rect").data(nodes);
   text = svg.selectAll("text").data(nodes);
-  // arrows = svg.append('svg:g').selectAll("path").data(edges);
   arrows = svg.selectAll("line").data(edges);
 }
 
@@ -178,7 +150,7 @@ function enterElem() {
          .append("rect")
          .attr("x", w)
          .attr("y", function(d) {
-           return d.yPos; // topY
+           return d.yPos;
          })
          .attr("width", square)
          .attr("height", square)
@@ -193,30 +165,15 @@ function enterElem() {
        })
        .attr("x", w + square/2)
        .attr("y", function(d) {
-         return d.yPos + square/ 2 + 7 ; // d.yPos = topY
+         return d.yPos + square/ 2 + 7 ;
        })
        .attr("font-family", "sans-serif")
        .attr("font-size", "20px")
        .attr("fill", "white")
        .attr("text-anchor", "middle");
-/*
-  arrows.enter()
-        .append("path")
-        .style("marker-start", "")
-        .style("marker-end", "url(#end-arrow)")
-        .style("stroke", "black")
-        .style("stroke-width", "5px")
-        .style("fill", "none")
-        .attr("d", function(d) {
-          var sourceX = d.source.xPos + square;
-          var sourceY = d.source.yPos + square / 2;
-          // var targetX = d.target.xPos;
-          // var targetY = d.target.yPos + square / 2;
-          return 'M' + sourceX + ',' + sourceY + 'L' + sourceX + ',' + sourceY;
-        });*/
+
   arrows.enter()
         .append("line")
-        // .style("marker-start", "")
         .style("marker-end", "url(#end-arrow)")
         .style("stroke", "black")
         .style("stroke-width", "5px")
@@ -242,7 +199,7 @@ function updateVisuals() {
            return d.xPos;
          })
          .attr("y", function(d) {
-           return d.yPos; // topY
+           return d.yPos;
          });
 
   text.transition()
@@ -254,26 +211,11 @@ function updateVisuals() {
         return d.xPos + square/2;
       })
       .attr("y", function(d) {
-        return d.yPos + square/ 2 + 7 ; // d.yPos = topY
+        return d.yPos + square/ 2 + 7 ;
       });
 
-/*
   arrows.transition()
         .duration(1000)
-        .attr("d", function(d) {
-          var sourceX = d.source.xPos + square;
-          var sourceY = d.source.yPos + square / 2;
-          var targetX = d.target.xPos;
-          var targetY = d.target.yPos + square / 2;
-          return 'M' + sourceX + ',' + sourceY + 'L' + targetX + ',' + targetY;
-        });
-        */
-  arrows.transition()
-        .duration(1000)
-        .style("marker-end", "url(#end-arrow)")
-        .style("stroke", "black")
-        .style("stroke-width", "5px")
-        .style("fill", "none")
         .attr("x1", function(d) {
           return d.source.xPos + square;
         })
@@ -288,81 +230,19 @@ function updateVisuals() {
         });
 }
 
-function loadElements1() {
-  elements = elements1;
-  // restart();
+function loadElements(version) {
+  if (version == 1) {
+    elements = elements1.slice();
+  } else if (version == 2) {
+    elements = elements2.slice();
+  } else if (version == 3) {
+    elements = elements3.slice();
+  } else {
+    elements = elements4.slice();
+  }
+
   convertData();
-  redraw();
-  console.log(elements);
-}
-
-function loadElements2() {
-  elements = elements2;
-  // restart();
-  convertData();
-  redraw();
-  console.log(elements);
-}
-
-function loadElements3() {
-  elements = elements3;
-  // restart();
-  convertData();
-  redraw();
-  console.log(elements);
-}
-
-function redraw() {
-  svg.selectAll("rect")
-    .data(nodes)
-    .transition()
-    .duration(1000)
-    .attr("x", function(d) {
-      return d.xPos;
-    })
-    .attr("y", function(d) {
-      return d.yPos; // topY
-    })
-    .attr("width", square)
-    .attr("height", square)
-    .attr("fill", function(d) {
-      return "rgb(0, 0, " + (d.value * 10) + ")";
-    });
-
-  svg.selectAll("text")
-    .data(nodes)
-    .transition()
-    .duration(1000)
-    .text(function(d) {
-      return d.value;
-    })
-    .attr("x", function(d) {
-      return d.xPos + square/2;
-    })
-    .attr("y", function(d) {
-      return d.yPos + square/ 2 + 7 ; // d.yPos = topY
-    })
-    .attr("font-family", "sans-serif")
-    .attr("font-size", "20px")
-    .attr("fill", "white")
-    .attr("text-anchor", "middle");
-
-  svg.selectAll("lines")
-    .data(edges)
-    .transition()
-    .duration(1000)
-    .attr("x1", function(d) {
-      return d.source.xPos + square;
-    })
-    .attr("y1", function(d) {
-      return d.source.yPos + square/2;
-    })
-    .attr("x2", function(d) {
-      return d.target.xPos;
-    })
-    .attr("y2", function(d) {
-      return d.target.yPos + square/2;
-    });
+  restart();
 }
 
 restart();
